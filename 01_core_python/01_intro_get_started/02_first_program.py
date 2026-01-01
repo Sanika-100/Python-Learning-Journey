44db3d127da6f9de2473074e3c8ad2eb728dc054
sanikaG <sanikaganorkar18@gmail.com>
2026-01-01 18:11:31 +0530

    Added Python intro and first program files

---
+++ 01_core_python/01_intro_get_started/02_first_program.py
@@ -0,0 +1,24 @@
+# First Python Program
+
+print("Hello, Python")
+print("Welcome to my Python learning journey")
+
+# Single-line comment example
+print("This is a simple print statement")
+
+"""
+Multi-line comment example
+Used for documentation and explanations
+"""
+
+print("Python executes code line by line")
+
+print("Learning Python")
+print("Step by step")
+print("With consistency")
+
+name = "Sanika"
+language = "Python"
+
+print("My name is", name)
+print("I am learning", language)
