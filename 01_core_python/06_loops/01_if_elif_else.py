32b22acd43b8c6d52ceec226ff196f9f4792e5aa
Sanika-100 <sanikaganorkar00@gmail.com>
2026-01-13 21:52:38 +0530

    Add basic if-elif-else examples

---
+++ 01_core_python/06_loops/01_if_elif_else.py
@@ -0,0 +1,15 @@
+# IF, ELIF, ELSE EXAMPLE
+
+score = 85
+
+if score >= 90:
+    print("Grade A")
+elif score >= 75:
+    print("Grade B")
+elif score >= 50:
+    print("Grade C")
+else:
+    print("Fail")
+
+# Output:
+# Grade B
