f3e3df4a3eb0caa27334eb692652fdda58883e62
Sanika-100 <sanikaganorkar00@gmail.com>
2026-01-13 21:56:59 +0530

    Add shorthand if and pass statement examples

---
+++ 01_core_python/06_loops/03_shorthand_and_pass.py
@@ -0,0 +1,20 @@
+# SHORTHAND IF
+
+number = 10
+print("Even") if number % 2 == 0 else print("Odd")
+
+# Output:
+# Even
+
+
+# PASS STATEMENT
+
+value = 5
+
+if value > 10:
+    pass  # will add logic later
+else:
+    print("Value is 10 or less")
+
+# Output:
+# Value is 10 or less
