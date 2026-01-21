ad0a9b246976ea0c325c97ff409a1627d13ec687
Sanika-100 <sanikaganorkar00@gmail.com>
2026-01-21 22:58:14 +0530

    Add functions basic examples

--- 01_core_python/07_functions/01_func_basics.py
+++ 01_core_python/07_functions/01_func_basics.py
@@ -40,6 +40,39 @@ def country_name(country="India"):
 country_name("Japan")
 country_name()
 
+#Return Statement
+#Square of a number.
+
+print('--------')
+def square(a =7):  #return a * a
+    return a ** 2
+
+
+result = square(20)
+print(result)
+
+
+#Count Vowels and consonants
+def count(userinput):
+    vowelsare = "AEIOUaeiou"
+    cntv = 0
+    cntc = 0
+
+    for eachchar in userinput:
+        if(eachchar.isalpha()):
+            if(eachchar in vowelsare):
+                cntv += 1
+            else:
+                cntc += 1
+
+    return cntv, cntc
+vowels, conso =  count("Sanik")
+print(vowels, conso)
+
+
 # Output:
 # Country: Japan
 # Country: India
+# --------
+# 400
+# 2 3
